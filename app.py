450f22655bc891b7eb1b951c5c4a15b01683cf4f
arun8nov <arunbabuprakash@gmail.com>
2025-11-13 09:49:08 +0530

    Ad tracking mapped into streamlit

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 import datetime
 import time
 from database import insert_match_data,Query,Query_a_Table
+from Ad_track import Ad_tracking
 
 st.set_page_config(page_title = "joihotstar Ads",
                    page_icon = "Jio",layout = "wide")
@@ -26,24 +27,40 @@ def MatchDataEntry():
     video_file = st.file_uploader("Upload match video", type=["mp4", "mov", "avi", "mkv"])
     if video_file is not None:
         st.info(f"Video will be saved to 'data/videos/{video_file.name}'")
-    video_path = f"data/videos/{video_file.name}" if video_file else ""
+    
   
 
     if st.button("Insert Data") and video_file is not None:
         if match_id and teams and location and match_type and winner and video_file :
+            # Folter create directory if not exists
+            folder_path = rf"data/{match_id}"
+            if not os.path.exists(folder_path):
+                os.makedirs(folder_path)
+            video_path = f"data/{match_id}/source_video.mp4"
+
             try:
                 # Insert data into the database
                 insert_match_data(match_id, teams, location, match_type, winner, video_path, match_timestamp)
+                
                 # Save the uploaded video file
                 with open(video_path, "wb") as f:
                     f.write(video_file.getbuffer())
                 st.success("Match data inserted successfully! and video saved.")
                 # Display the uploaded video
                 st.video(video_file)
+                # Run Ad Tracking
+                with st.spinner("Running Ad Tracking..."):
+                    Ad_tracking(video_path)
+                st.success("Ad Tracking completed and results saved.")
+                
             except Exception as e:
                 st.error(f"Error inserting data: {e}")
         else:
             st.error("Please fill out all required fields.")
+
+
+
+
     st.subheader("Current Tables in Database")
     tables = Query("SHOW TABLES;")
     st.text(','.join([table[0] for table in tables]))
